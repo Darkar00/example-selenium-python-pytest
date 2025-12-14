42a0f828994f6e3c1fb74ea81a8922c09bd1279a
Darkar00 <jacob22032@gmail.com>
2025-12-14 01:02:02 -0500

    Update test_acme_bank.py

--- tests/test_acme_bank.py
+++ tests/test_acme_bank.py
@@ -5,17 +5,6 @@ from selenium.webdriver.common.by import By
 
 def test_log_into_bank_account(webdriver: Chrome, eyes: Eyes) -> None:
 
-  # Load the login page.
-  webdriver.get("https://demo.applitools.com")
-
-  # Verify the full login page loaded correctly.
-  eyes.check(Target.window().fully().with_name("Login page"))
-
-  # Perform login.
-  webdriver.find_element(By.ID, "username").send_keys("applibot")
-  webdriver.find_element(By.ID, "password").send_keys("I<3VisualTests")
-  webdriver.find_element(By.ID, "log-in").click()
-
-  # Verify the full main page loaded correctly.
-  # This snapshot uses LAYOUT match level to avoid differences in closing time text.
+  # Load the main page.
+  webdriver.get("https://demo.saleor.io/default-channel")
   eyes.check(Target.window().fully().with_name("Main page").layout())
